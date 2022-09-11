a5ca905995a3d2ca9fde3e25647a082e1481a446
SteinS <novaldisago@gmail.com>
2022-09-11 10:29:43 +0700

    add env port import

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const expenditureRouter = require("./routes/expenditure");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const db =
   "mongodb+srv://steins:steins@cluster0.ajqocff.mongodb.net/?retryWrites=true&w=majority";
 
